351fdf6d8ed5d6fb8195c4fdc36980715a725d8b
Andy <andy.lee.surfer@gmail.com>
2016-09-08 20:17:27 +0000

    Fixed timeout bug

--- server/devices/steering.js
+++ server/devices/steering.js
@@ -56,7 +56,7 @@ console.log(command.direction + ' for ' + command.timeOn);
   stop() {
     // Stop any current movement
     if (this.timer) {
-      this.timer.clearTimeout();
+      clearTimeout(this.timer);
       this.timer = null;
     }
 
